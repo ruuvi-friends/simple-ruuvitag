c50af5651c5ff669f3a18fc29a3bbed4956453cb
sergioisidoro <smaisidoro@gmail.com>
2021-03-28 20:23:12 +0300

    Add RSSI information to payload

--- simple_ruuvitag/adaptors/dummy.py
+++ simple_ruuvitag/adaptors/dummy.py
@@ -11,14 +11,20 @@ class DummyBle(BluetoothAdaptor):
         callback(
             {
                 "address": 'DU:MM:YD:AT:A9:3D',
-                "raw_data": '1E0201060303AAFE1616AAFE10EE037275752E76692F23416A7759414D4663CD'
+                "raw_data": '1E0201060303AAFE1616AAFE10EE037275752E76692F23416A7759414D4663CD',
+                "tx_power": 0,
+                "rssi": -99,
+                "name": 'bleson',
             }
         )
 
         callback(
             {
                 "address": 'NO:TS:UP:PO:RT:ED',
-                "raw_data": '1E0201060303AAFE1616AAFE10EE037275752E76692F23416A7759414D4663CD'
+                "raw_data": '1E0201060303AAFE1616AAFE10EE037275752E76692F23416A7759414D4663CD',
+                "tx_power": 0,
+                "rssi": -99,
+                "name": 'bleson',
             }
         )
 
